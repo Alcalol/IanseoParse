15075525e8a6e116ef1259d0ce24f2efa9f9536c
Tony Sze <szetech@gmail.com>
2019-08-08 18:06:07 +0100

    Updated and merged code for less code duplication and better efficiency

--- src/main/java/IanseoParse.java
+++ src/main/java/IanseoParse.java
@@ -81,6 +81,8 @@ public class IanseoParse {
         }
         else{
             try{
+
+                //Multi address file provide, cycle through each line and process events
                 File file = new File(addressInfo);
 
                 BufferedReader buffer = new BufferedReader(new FileReader(file));
@@ -88,6 +90,7 @@ public class IanseoParse {
                 String eventURL;
 
                 while((eventURL = buffer.readLine()) != null){
+
                     ProcessEvent(outputPath, eventURL, writeHeaders);
                 }
 
@@ -104,11 +107,19 @@ public class IanseoParse {
     }
 
     public static void ProcessEvent(String filePath, String url, boolean writeHeaders){
+        //Extract event code from event URL
         String eventCode = url.split("=")[1];
+        System.out.println("Procesing event " + eventCode);
 
+        //Hashmap to store all qualification and bracket categories inside current event
+        //DetectEventDiscipline searches for all categories listed within the event page.
         HashMap<String, String> linksToParse = DetectEventDisciplines(url);
 
         linksToParse.forEach((k, v) -> {
+            //Process each category individually
+
+            //First check path is legit, if the path to save contains any type of slashes at the end, remove.
+            //This path is the raw save path only, does not include file name.
             String fullPath = "";
 
             if(filePath.equals("")){
@@ -122,26 +133,34 @@ public class IanseoParse {
             }
 
             if(k.contains("Qualification")){
+                //If the event is a qualification, call the appropriate processor
                 System.out.println(k);
                 ParseAndSaveQualification(fullPath, eventCode + "-" + k + ".csv", v, writeHeaders);
             }
             else if (k.contains("Brackets")){
+                //If event is a brackets (H2H), call the appropriate processor
                 System.out.println(k);
                 if(k.contains("Individual")){
 
                     ParseAndSaveIndividualBrackets(fullPath, eventCode + "-" + k + ".csv", v, writeHeaders);
                 }
                 else{
-                    //Team
+                    //Team - To be implemented
+
                 }
             }
             else if(k.contains("Round_Only")){
+                //Round only event, like 1440, call the appropriate processor
                 ParseAndSaveRoundOnly(fullPath, eventCode, v, writeHeaders);
             }
         });
     }
 
     public static HashMap<String, String> DetectEventDisciplines(String url){
+        //This function finds all the categories available in provided event
+        //Note: Junior only rounds currently not implemented (There sre so many!!)
+
+        //Hashmap listing all possible rounds that are currently supported
         HashMap<String, String> output = new HashMap<>();
         String[] disciplinesArray = {"IC.php", "IQRM.php", "IQRW.php", "IQCM.php", "IQCW.php", "IQBM.php", "IQBW.php", "IQLM.php", "LQLW.php", "IBRM.php", "IBRW.php", "IBCM.php", "IBCW.php", "IBBM.php", "IBBW.php", "IBLM.php", "IBLW.php", "TQRM.php", "TQRW.php", "TQCM.php", "TQCW.php", "TQBM.php", "TQBW.php", "TQLM.php", "TQLW.php", "TBRM.php", "TBRW.php", "TBCM.php", "TBCW.php", "TBBM.php", "TBBW.php", "TBLM.php", "TBLW.php"};
 
@@ -149,9 +168,13 @@ public class IanseoParse {
 
             Document doc = Jsoup.connect(url).get();
 
+            //Search HTML for all links
             Elements linkTags = doc.getElementsByTag("a");
 
             linkTags.forEach(t -> {
+
+                //In each link, see if the linked page contains any of the supported category codes
+                //If so, add category to list
 
                 if (Arrays.stream(disciplinesArray).parallel().anyMatch(t.toString()::contains)){
                     String linkPath = t.toString().split("\"")[1];
@@ -212,7 +235,7 @@ public class IanseoParse {
                         }
                     }
 
-
+                    //Only add to list if the event is legit
                     if(!(eventType.equals("Error") || eventType.equals("") || discipline.equals("Error") || discipline.equals(""))){
                         output.put(eventType  + "-" + discipline, "https://ianseo.net" + linkPath);
                     }
@@ -234,33 +257,11 @@ public class IanseoParse {
         try{
             Document doc = Jsoup.connect(url).get();
 
+            //Extract Accordion object
             Element accordion = doc.getElementById("Accordion");
-            Element wholeTable = accordion.getElementsByClass("Griglia").first();
 
-            Elements competitors = wholeTable.getElementsByClass("row1");
-
-            ArrayList<String[]> csvArray = new ArrayList<>();
-
-            if(writeHeaders){
-                String[] csvHeader = {"Rank", "Name", "Country", "Score", "10 + X", "X Only"};
-                csvArray.add(csvHeader);
-            }
-
-            competitors.forEach(c -> {
-
-                String rank = c.getElementsByClass("Rank").get(0).text();
-                String name = c.getElementsByClass("Athlete").get(0).text();
-                String countryCode = c.getElementsByClass("CoCode").get(0).text();
-                String finalScore = c.getElementsByClass("Score").get(0).text();
-                String golds = c.getElementsByClass("Golds").get(0).text();
-                String xCount = c.getElementsByClass("Golds").get(1).text();
-
-                String[] csvRow = {rank, name, countryCode, finalScore, golds, xCount};
-                csvArray.add(csvRow);
-
-            });
-
-            WriteToCsv(filePath, fileName, csvArray);
+            //Processor is shared with Round only because format is the same
+            ProcessQualificationAccordion(filePath, fileName, accordion.getElementsByClass("accordion").first(),writeHeaders);
 
         }
         catch (IOException ex){
@@ -275,6 +276,7 @@ public class IanseoParse {
 
             Document doc = Jsoup.connect(url).get();
 
+            //Round only has multiple inner accordions per category, nested without two outer accordions
             Element firstAccordion = doc.getElementById("Accordion");
 
             Element secondAccordion = doc.getElementById("Accordion");
@@ -282,11 +284,15 @@ public class IanseoParse {
             Elements innerAccordions = doc.getElementsByClass("accordion");
 
             for(int i = 0; i < innerAccordions.size(); i++){
+                //Loop through all inner accordions, thereby processing each category individually
+
                 System.out.println(innerAccordions.get(i).getElementsByClass("title").get(0).text().replace(" ", "").split("\\[")[0]);
 
+                //Set file name to output, only needed in this mode because category is unknown until processing
                 fileName = eventCode + "-RoundOnly-" + innerAccordions.get(i).getElementsByClass("title").get(0).text().replace(" ", "").split("\\[")[0] + ".csv";
 
-                ProcessRoundOnlyAccordion(filePath, fileName, innerAccordions.get(i), writeHeaders);
+                //Process current category and write to CSV
+                ProcessQualificationAccordion(filePath, fileName, innerAccordions.get(i), writeHeaders);
             }
         }
         catch(IOException e){
@@ -294,8 +300,11 @@ public class IanseoParse {
         }
     }
 
-    public static void ProcessRoundOnlyAccordion(String filePath, String fileName, Element accordion, boolean writeHeaders){
+    public static void ProcessQualificationAccordion(String filePath, String fileName, Element accordion, boolean writeHeaders){
 
+            //Inner accordion structure is the same for both Qualification AND Round Only types
+
+            //Extract rows within Griglia class table.
             Element table = accordion.getElementsByClass("Griglia").get(0);
             Elements competitors = table.getElementsByTag("tr");
 
@@ -308,6 +317,8 @@ public class IanseoParse {
             }
 
             for(int i = 1; i < competitors.size(); i++){
+                //Cycle through per class extracting wanted information
+
                 String rank = competitors.get(i).getElementsByClass("Rank").get(0).text();
                 String name = competitors.get(i).getElementsByClass("Athlete").get(0).text();
                 String country = competitors.get(i).getElementsByClass("CoCode").get(0).text();
@@ -316,9 +327,12 @@ public class IanseoParse {
                 String xCount = competitors.get(i).getElementsByClass("Golds").get(1).text();
 
                 String[] row = {rank, name, country, score, golds, xCount};
+
+                //Add collected row to list
                 csvRows.add(row);
             }
 
+            //Write completed CSV row list to file
             WriteToCsv(filePath, fileName, csvRows);
     }
 
@@ -326,14 +340,18 @@ public class IanseoParse {
         try{
             Document doc = Jsoup.connect(url).get();
 
+            //First extract accordion, then extract Griglia table
             Element accordion = doc.getElementById("Accordion");
             Element wholeTable = accordion.getElementsByClass("Griglia").first();
 
+            //Extract table rows inside Griglia
             Elements resultRows = wholeTable.getElementsByTag("tr");
 
+            //Extract header data from first row to acquire pass names
             Elements bracketsDef = resultRows.get(0).getElementsByTag("th");
             ArrayList<String> passNameList = new ArrayList<>();
 
+            //Add pass names to list for later use
             bracketsDef.forEach(th -> {
                 passNameList.add(th.getElementsByTag("div").get(0).text());
             });
@@ -352,39 +370,39 @@ public class IanseoParse {
                 for(int j = 0; j < roundsCount; j++){
                     //Loop through each column in table
                     //First pass - 8 columns
-                    //All subsequent passes except finals - 5 columns
-                    //Finals 2 columns
+                    //All subsequent passes - 5 columns
 
                     Elements currentRow = resultRows.get(i).getElementsByTag("td");
 
                     if(j == 0){
-                        if (!currentRow.get(1).text().equals("") && !currentRow.get(5).text().equals("Bye")){
+                        //If j ==0, info is first pass, skip byes and empty rows
+                        if (!currentRow.get(1).text().equals("") && !currentRow.get(5).text().equals("Bye") && !currentRow.get(5).text().equals("")){
                             //Not blank, has player data
                             String[] tempCompetitor = {currentRow.get(1).text(), currentRow.get(5).text()};
 
+                            //Store competitor in correct pass list
                             matchesStorage.get(j).add(tempCompetitor);
                         }
                     }
                     else{
-                        if(j < roundsCount - 1){
-                            int targetColumn = 7 + (5 * (j - 1)) + 2;
+                        //All subsequent passes, with field column calcualted as below
+                        int targetColumn = 7 + (5 * (j - 1)) + 2;
 
-                            if (!currentRow.get(targetColumn).text().equals("") && !currentRow.get(targetColumn + 1).text().equals("Bye")){
-                                //Not blank, has player data
-                                String[] tempCompetitor = {currentRow.get(targetColumn).text(), currentRow.get(targetColumn + 1).text()};
+                        if (!currentRow.get(targetColumn).text().equals("") && !currentRow.get(targetColumn + 1).text().equals("Bye") && !currentRow.get(targetColumn + 1).text().equals("")){
+                            //Not blank, has player data
+                            String[] tempCompetitor = {currentRow.get(targetColumn).text(), currentRow.get(targetColumn + 1).text()};
 
+                            if(j == roundsCount - 1){
+                                //Finals, add in reverse order (because Ianseo displays Gold before Bronze)
+                                //Store competitor in correct pass list
+                                matchesStorage.get(j).add(0, tempCompetitor);
+                            }
+                            else{
+                                //Store competitor in correct pass list
                                 matchesStorage.get(j).add(tempCompetitor);
                             }
                         }
-                        else{
-                            //Finals
-                            if(!currentRow.get(currentRow.size() - 9).text().equals("Final") && !currentRow.get(currentRow.size() - 9).text().equals("Gold") && !currentRow.get(currentRow.size() - 9).text().equals("Bronze") && !currentRow.get(currentRow.size() - 9).text().equals("") && !currentRow.get(currentRow.size() - 8).text().equals("Bye")){
-                                String[] tempCompetitor = {currentRow.get(currentRow.size() - 9).text(), currentRow.get(currentRow.size() - 8).text()};
 
-                                matchesStorage.get(j).add(0, tempCompetitor);
-                                //matchesStorage.get(j).add(tempCompetitor);
-                            }
-                        }
                     }
                 }
             }
@@ -399,11 +417,17 @@ public class IanseoParse {
             }
 
             for(int i = 0; i < passNameList.size(); i++){
-                //Loop through passes
+                //Loop through each pass
                 String passName = passNameList.get(i);
 
                 if(matchesStorage.get(i).size() >= 2){
+                    //In each pass, couple current entry and next entry as a H2H pair.
+                    //This works because byes and empty fields are not added to this list.
+                    //So there should always be an even number of competitors and every
+                    //2 items in the list should be the same H2H match
                     for(int j = 0; j < matchesStorage.get(i).size() - 1; j += 2){
+
+                        //If final pass, label matches according to current j index
                         if(i == passNameList.size() - 1 && j == 0){
                             passName = "Bronze";
                         }
@@ -414,6 +438,7 @@ public class IanseoParse {
                         int aWin = 0;
                         int bWin = 0;
 
+                        //Detect stars in score box to determin winner for matches with shoot-off
                         if(matchesStorage.get(i).get(j)[1].contains("*")){
                             aWin = 1;
                         }
@@ -432,14 +457,17 @@ public class IanseoParse {
                             }
                         }
 
+                        //Save deets to array
                         String[] matchDeets = {passName, matchesStorage.get(i).get(j)[0], matchesStorage.get(i).get(j)[1], aWin + "", matchesStorage.get(i).get(j + 1)[0], matchesStorage.get(i).get(j + 1)[1], bWin + ""};
 
+                        //Add array to csv list for saving
                         csvArray.add(matchDeets);
                     }
                 }
 
             }
 
+            //CSV row list written, save to file
             WriteToCsv(filePath, fileName, csvArray);
 
         }
